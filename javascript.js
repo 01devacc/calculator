b666b280130c0efd4f1d0dd3a2e08b001e45ff15
Ahmed <202986375+01devacc@users.noreply.github.com>
2025-08-07 19:42:28 +0100

    Add operate function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let num1;
+let num2;
+let operator;
+
 function add(num1, num2) {
     return num1 + num2
 }
@@ -12,4 +16,17 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2
+}
+
+function operate(num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            add(num1, num2)
+        case "-":
+            subtract(num1, num2)
+        case "*":
+            multiply(num1, num2)
+        case "/":
+            divide(num1, num2)
+    }
 }
