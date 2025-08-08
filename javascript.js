0165d7a1ba848cc4bd22268b0576f3b38b65bd56
Ahmed <202986375+01devacc@users.noreply.github.com>
2025-08-08 02:29:04 +0100

    Add functions and calculation loop

--- javascript.js
+++ javascript.js
@@ -1,8 +1,16 @@
 let num1;
 let num2;
 let operator;
+let operatePrimed = false;
+
+let firstOperand = '';
+let secondOperand = '';
+
 let display = document.querySelector('.display')
 let digits = document.querySelectorAll('.digit')
+let operators = document.querySelectorAll('.operator')
+let equal = document.querySelector('.equals')
+let clear = document.querySelector('.clear')
 
 function add(num1, num2) {
     return num1 + num2
@@ -26,7 +34,7 @@ function operate(num1, num2, operator) {
             return add(num1, num2)
         case "-":
             return subtract(num1, num2)
-        case "*":
+        case "x":
             return multiply(num1, num2)
         case "/":
             return divide(num1, num2)
@@ -38,9 +46,78 @@ function operate(num1, num2, operator) {
 function populateDisplay() {
     digits.forEach(digit => {
         digit.addEventListener('click', () => {
-            display.textContent = digit.textContent
+            if ((display.textContent === '0' || display.textContent === '') && digit.textContent === '0') {
+                display.textContent = 0;
+                
+            } else if (operatePrimed) {
+                secondOperand += digit.textContent;
+                display.textContent = secondOperand;
+
+            } else {    
+                firstOperand += digit.textContent;
+                display.textContent = firstOperand;
+            }
         })
     })
 }
 
-populateDisplay()
+function selectOperator() {
+    operators.forEach(operatorButton => {
+        operatorButton.addEventListener('click', () => {
+            
+            if (operatePrimed) {
+                    let result = calculate();
+                    displayResult(result)
+                }
+            operator = operatorButton.textContent;
+            operatePrimed = true;
+        })
+
+        })
+}
+
+
+function equalsPressed() {
+    if (true) {
+        equal.addEventListener('click', () => {
+            let result = calculate();
+            displayResult(result)
+            
+        })
+    }
+}
+
+function convertToNumbers(firstOperand, secondOperand) {
+    num1 = Number(firstOperand);
+    num2 = Number(secondOperand); 
+}
+
+function displayResult(result) {
+    display.textContent = result.toString();
+    firstOperand = result.toString();
+    secondOperand = '';
+}
+
+
+function calculate() {
+    convertToNumbers(firstOperand, secondOperand);
+    return operate(num1, num2, operator);
+}
+
+function clearPressed() {
+    clear.addEventListener('click', () => {
+        display.textContent = '';
+        firstOperand = '';
+        secondOperand = '';
+        operatePrimed = false;
+    })
+}
+
+function mainLoop() {
+    populateDisplay()
+    selectOperator()
+    equalsPressed()
+    clearPressed()
+}
+
+mainLoop();
